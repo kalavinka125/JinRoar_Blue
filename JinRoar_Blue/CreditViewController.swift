08543de993521015eebd34cff3544bea16afc4ee
kalavinka3190@gmail.com <sotuken@admin-no-MacBook-Air-2.local>
2017-12-03 13:04:32 +0900

    クレジット画面の実装、テーブルビューの設定終了

--- JinRoar_Blue/CreditViewController.swift
+++ JinRoar_Blue/CreditViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class CreditViewController: UIViewController {
+class CreditViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
 
     @IBOutlet weak var tableView: UITableView!
+    
+    let CELL_ID = "CREDIT_CELL"
+    private let sections : [String] = ["　開発者","　素材提供"]
+    private let staffList:[Credit] = [Credit(name: "宮脇流士", position: "プロデューサー"),Credit(name: "秋山裕太", position: "プランナー"),Credit(name: "宇崎裕太", position: "クリエイター"),Credit(name: "瀬尾敦生", position: "テクニカルサポート"),Credit(name: "香川聖雄", position: "デバッガー")]
+    private let specialThx:[Credit] = [Credit(name:"テスト",position:"テスト"),Credit(name:"テスト",position:"テスト"),Credit(name:"テスト",position:"テスト")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +43,60 @@ class CreditViewController: UIViewController {
     }
     */
     @IBAction func backButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return self.staffList.count
+        }else if section == 1 {
+            return self.specialThx.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.CELL_ID, for: indexPath) as! CreditTableViewCell
+        cell.nameLabel.adjustsFontSizeToFitWidth = true
+        cell.positionLabel.adjustsFontSizeToFitWidth = true
+        
+        
+        if indexPath.section == 0 {
+            cell.nameLabel.text = self.staffList[indexPath.row].name
+            cell.positionLabel.text = self.staffList[indexPath.row].position
+        }else if indexPath.section == 1 {
+            cell.nameLabel.text = self.specialThx[indexPath.row].name
+            cell.positionLabel.text = self.specialThx[indexPath.row].position
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        let font = UIFont(name: "PixelMplus10-Regular", size: 17.0)
+        label.font = font
+        label.textColor = UIColor.black
+        label.backgroundColor = #colorLiteral(red: 1, green: 0.4352941176, blue: 0.8117647059, alpha: 1)
+        label.adjustsFontSizeToFitWidth = true
+        if section == 0 {
+            label.text = self.sections[section]
+        }else if section == 1 {
+            label.text = self.sections[section]
+        }
+        return label
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 32.0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 64.0
+    }
 }
