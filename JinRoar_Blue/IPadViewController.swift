3ca23405649b8d7878592ffc9855359b8f5030a8
kalavinka3190@gmail.com <sotuken@admin-no-MacBook-Air-2.local>
2017-12-13 12:26:43 +0900

    iPad用メイン画面ViewController追加

---
+++ JinRoar_Blue/IPadViewController.swift
@@ -0,0 +1,36 @@
+//
+//  IPadViewController.swift
+//  JinRoar_Blue
+//
+//  Created by sotuken on 2017/12/07.
+//  Copyright © 2017年 sotuken. All rights reserved.
+//
+
+import UIKit
+
+class IPadViewController: UIViewController {
+
+    @IBOutlet weak var descriptionTebleView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
